6c3f5c21f1fcceb6b444dd83feabfbe7ff1abbb1
Lukas <Lukas.dybkjaer@gmail.com>
2020-11-20 12:27:26 +0100

    Save 0.1

--- lib/main.dart
+++ lib/main.dart
@@ -126,6 +126,12 @@ class _MyHomePageState extends State<MyHomePage> {
         .then((value) => setState(() {}));
   }
 
+  void _navigateToNewScreen(BuildContext context, Person person) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => EditScreen(person)))
+        .then((value) => setState(() {}));
+  }
+
   Widget display(BuildContext context, Person person) {
     return Container(
         child: Column(
@@ -193,8 +199,25 @@ class _MyHomePageState extends State<MyHomePage> {
                 alignment: Alignment.topRight),
           ],
         ),
+        FlatButton(
+            child: Text(
+              'Rediger person',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onPressed: () {
+              _navigateToNewScreen(context, person);
+            })
       ],
     ));
+  }
+}
+
+class EditScreen extends StatefulWidget {
+  Person person;
+  List persons;
+  EditScreen(this.person);
+  State<StatefulWidget> createState() {
+    return MyCustomForm2();
   }
 }
 
@@ -304,7 +327,7 @@ class MyCustomForm extends State<NewScreen> {
                     }),
                 ElevatedButton(
                   child: Text(
-                    'Submit',
+                    'gem',
                     style: TextStyle(fontSize: 24.0),
                   ),
                   onPressed: () async {
@@ -331,5 +354,107 @@ class MyCustomForm extends State<NewScreen> {
                 ),
               ],
             )));
+  }
+}
+
+class MyCustomForm2 extends State<EditScreen> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _navnController = TextEditingController();
+  TextEditingController _alderController = TextEditingController();
+  TextEditingController _jobController = TextEditingController();
+  TextEditingController _retController = TextEditingController();
+
+  MyCustomForm2() {}
+
+  @override
+  void initState() {
+    _navnController.text = widget.person.navn;
+    _alderController.text = widget.person.alder;
+    _jobController.text = widget.person.job;
+    _retController.text = widget.person.ynglingsret;
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('New Screen')),
+        body: Form(
+            key: _formKey,
+            child: Column(children: <Widget>[
+              TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  controller: _navnController,
+                  decoration: InputDecoration(
+                    hintText: 'navn',
+                    counterText: '0 characters',
+                    border: const OutlineInputBorder(),
+                  )),
+              TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  // The validator receives the text that the user has entered.
+                  controller: _alderController,
+                  decoration: InputDecoration(
+                    hintText: 'Alder',
+                    counterText: '0 characters',
+                    border: const OutlineInputBorder(),
+                  )),
+              TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  controller: _jobController,
+                  decoration: InputDecoration(
+                    hintText: 'Job',
+                    counterText: '0 characters',
+                    border: const OutlineInputBorder(),
+                  )),
+              TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  controller: _retController,
+                  decoration: InputDecoration(
+                    hintText: 'Ynglingsret',
+                    counterText: '0 characters',
+                    border: const OutlineInputBorder(),
+                  ) // Add TextFormFields and ElevatedButton here.
+                  ),
+              ElevatedButton(
+                  child: Text(
+                    'gem',
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                  onPressed: () async {
+                    widget.person.navn = _navnController.text;
+                    widget.person.alder = _alderController.text;
+                    widget.person.job = _jobController.text;
+                    widget.person.ynglingsret = _retController.text;
+                    print(jsonEncode(widget.person));
+                    final filename = join(
+                        (await getApplicationDocumentsDirectory()).path,
+                        'persons.json');
+                    File file = File(filename);
+                    file.writeAsString(jsonEncode(widget.person));
+                    Navigator.pop(context);
+                  })
+            ])));
   }
 }
--- lib/models/profilemodel.dart
+++ lib/models/profilemodel.dart
@@ -1,10 +1,10 @@
 class Person {
   final String firma;
-  final String billed;
-  final String navn;
-  final String alder;
-  final String job;
-  final String ynglingsret;
+  String billed;
+  String navn;
+  String alder;
+  String job;
+  String ynglingsret;
   Person(this.firma, this.billed, this.navn, this.alder, this.job,
       this.ynglingsret);
 
